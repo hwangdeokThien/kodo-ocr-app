08845e5be9cd6db8bf6979830845c20d99390f1b
hwangdeokthien <huynhducthien41906@gmail.com>
2024-09-15 17:57:47 +0700

    [UPDATE] api for scan

--- backend/src/router/photo.ts
+++ backend/src/router/photo.ts
@@ -19,11 +19,12 @@ async function scanPhoto(body: any) {
     try {
         const formData = new FormData()
         formData.append('image', body.photo, 'image.jpg')
-        formData.append('req_struct', 'false')
-        formData.append('llm_aided', 'true')
+        formData.append('req_struct', body.req_struct)
+        formData.append('llm_aided', body.llm_aided)
 
         // Send the request to the Flask API
-        const reg_det_text = await fetch('http://127.0.0.1:5000/scan', {
+        const ai_api = process.env.AI_BASE_API || 'http://127.0.0.1:5000'
+        const reg_det_text = await fetch(ai_api + '/scan', {
             method: 'POST',
             body: formData
         })
@@ -87,6 +88,7 @@ async function deletePhoto(id: string) {
 
 const photoRoutes = new Elysia({ prefix: "/photos" })
     .get("/:id", ({params: {id}, body}) => getPhoto(id, body))
+    .post("/save", ({ body }) => savePhoto(body), { type: 'multipart/form-data' })
     .post("/scan", ({ body }) => scanPhoto(body), { type: 'multipart/form-data' })
     .delete("/:id", ({ params: { id } }) => deletePhoto(id));
 
